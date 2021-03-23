f41f50952ea7dc7c8dc37fe162bbbc256e4ecc1c
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2021-03-23 11:45:04 -0500

    wip

---
+++ src/main/java/org/xjrga/palette/Barragan00.java
@@ -0,0 +1,18 @@
+package org.xjrga.palette;
+
+import java.awt.Color;
+
+public class Barragan00 {
+
+    public static final String ARTWORK = "Luis Barragan Color Palette";  
+    public static final Color GREEN = new Color(0xbb2d81);
+    public static final Color PINK = new Color(0xdb5027);
+    public static final Color LIGHTBLUE = new Color(0xe7ba17);
+    public static final Color MEDIUMBLUE = new Color(0xb01510);
+    public static final Color DARKBLUE = new Color(0x2caaff);
+    
+    public Barragan00() {
+
+    }
+   
+}
