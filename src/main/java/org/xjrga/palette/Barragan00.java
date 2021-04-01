1c64083b797bfe5c12f96767962226b0e14043a9
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2021-04-01 09:54:37 -0500

    wip

--- src/main/java/org/xjrga/palette/Barragan00.java
+++ src/main/java/org/xjrga/palette/Barragan00.java
@@ -5,11 +5,11 @@ import java.awt.Color;
 public class Barragan00 {
 
     public static final String ARTWORK = "Luis Barragan Color Palette";  
-    public static final Color GREEN = new Color(0xbb2d81);
-    public static final Color PINK = new Color(0xdb5027);
-    public static final Color LIGHTBLUE = new Color(0xe7ba17);
-    public static final Color MEDIUMBLUE = new Color(0xb01510);
-    public static final Color DARKBLUE = new Color(0x2caaff);
+    public static final Color PINK = new Color(0xbb2d81);
+    public static final Color ORANGE = new Color(0xdb5027);
+    public static final Color YELLOW = new Color(0xe7ba17);
+    public static final Color RED = new Color(0xb01510);
+    public static final Color BLUE = new Color(0x2caaff);
     
     public Barragan00() {
 
--- src/main/java/org/xjrga/palette/Barragan02.java
+++ src/main/java/org/xjrga/palette/Barragan02.java
@@ -2,16 +2,16 @@ package org.xjrga.palette;
 
 import java.awt.Color;
 
-public class Barragan00 {
+public class Barragan02 {
 
-    public static final String ARTWORK = "Luis Barragan Color Palette";  
-    public static final Color GREEN = new Color(0xbb2d81);
-    public static final Color PINK = new Color(0xdb5027);
-    public static final Color LIGHTBLUE = new Color(0xe7ba17);
-    public static final Color MEDIUMBLUE = new Color(0xb01510);
-    public static final Color DARKBLUE = new Color(0x2caaff);
+    public static final String ARTWORK = "Luis Barragan Home and Studio";  
+    public static final Color PINK = new Color(0xf8b1d3);    
+    public static final Color ORANGE = new Color(0xd74b28);
+    public static final Color PURPLE = new Color(0x83534f);
+    public static final Color WHITE = new Color(0xf0f1eb);
+    public static final Color BLUE = new Color(0x8894ac);
     
-    public Barragan00() {
+    public Barragan02() {
 
     }
    
