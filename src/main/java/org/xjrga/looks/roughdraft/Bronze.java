c6d6c0abfdf51dd1253a2e4a793839b5f37ef6a0
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2021-04-08 10:48:03 -0500

    wip

---
+++ src/main/java/org/xjrga/looks/roughdraft/Bronze.java
@@ -0,0 +1,121 @@
+package org.xjrga.looks.roughdraft;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.UIDefaults;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.FontUIResource;
+import javax.swing.plaf.metal.MetalTheme;
+
+/**
+ * This class is a theme for Java Look and Feel (also known as Metal)
+ *
+ * @author Jorge R Garcia de Alba
+ */
+public class Bronze extends MetalTheme {
+
+    private final ColorUIResource primary1;
+    private final ColorUIResource primary2;
+    private final ColorUIResource primary3;
+    private final ColorUIResource secondary1;
+    private final ColorUIResource secondary2;
+    private final ColorUIResource secondary3;
+    private final Color COLOR00 = new Color(0xffcc7e);
+    private final Color COLOR10 = new Color(0xe6b871);
+    private final Color COLOR20 = new Color(0xcca365);
+    private final Color COLOR30 = new Color(0xb38f58);
+    private final Color COLOR40 = new Color(0x997b4c);
+    private final Color COLOR50 = new Color(0x80663f);
+    private final Color COLOR60 = new Color(0x665232);
+    private final Color COLOR70 = new Color(0x4d3d26);
+    private final Color COLOR80 = new Color(0x332919);
+    private final Color COLOR90 = new Color(0x19140d);
+    private final Color COLOR100 = new Color(0x000000);
+    private final FontUIResource fontUIResource;
+
+    /**
+     * Bronze constructor
+     *
+     * @param font the font
+     * @see java.awt.Font
+     */
+    public Bronze(Font font) {
+        fontUIResource = new FontUIResource(font);
+        secondary3 = new ColorUIResource(this.COLOR40);
+        secondary2 = new ColorUIResource(this.COLOR40);
+        primary3 = new ColorUIResource(this.COLOR20);
+        primary1 = new ColorUIResource(this.COLOR00);
+        secondary1 = new ColorUIResource(this.COLOR30);
+        primary2 = new ColorUIResource(this.COLOR20);//        
+    }
+
+    @Override
+    public void addCustomEntriesToTable(UIDefaults table) {
+        super.addCustomEntriesToTable(table);
+    }
+
+    @Override
+    public String getName() {
+        return "Bronze";
+    }
+
+    @Override
+    public ColorUIResource getPrimary1() {
+        return primary1;
+    }
+
+    @Override
+    public ColorUIResource getPrimary2() {
+        return primary2;
+    }
+
+    @Override
+    public ColorUIResource getPrimary3() {
+        return primary3;
+    }
+
+    @Override
+    public ColorUIResource getSecondary1() {
+        return secondary1;
+    }
+
+    @Override
+    public ColorUIResource getSecondary2() {
+        return secondary2;
+    }
+
+    @Override
+    public ColorUIResource getSecondary3() {
+        return secondary3;
+    }
+
+    @Override
+    public FontUIResource getControlTextFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getSystemTextFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getUserTextFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getMenuTextFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getWindowTitleFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getSubTextFont() {
+        return fontUIResource;
+    }
+}
---
+++ src/main/java/org/xjrga/looks/roughdraft/Khaki.java
@@ -0,0 +1,120 @@
+package org.xjrga.looks.roughdraft;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Insets;
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.FontUIResource;
+import javax.swing.plaf.metal.MetalTheme;
+
+/**
+ * This class is a theme for Java Look and Feel (also known as Metal)
+ *
+ * @author Jorge R Garcia de Alba
+ * @see java.awt.Font
+ */
+public class Khaki extends MetalTheme {
+
+    private final ColorUIResource primary1;
+    private final ColorUIResource primary2;
+    private final ColorUIResource primary3;
+    private final ColorUIResource secondary1;
+    private final ColorUIResource secondary2;
+    private final ColorUIResource secondary3;
+    public final Color A = new Color(0xac5c2c);
+    public final Color B = new Color(0xc8a353);
+    public final Color C = new Color(0x281924);
+    public final Color D = new Color(0x897846);
+    public final Color E = new Color(0x9ba068);
+    public final Color F = new Color(0xe0cca8);
+    private final FontUIResource fontUIResource;
+
+    /**
+     * Khaki constructor
+     *
+     * @param font the font
+     * @see java.awt.Font
+     */
+    public Khaki(Font font) {
+        fontUIResource = new FontUIResource(font);
+        secondary3 = new ColorUIResource(this.F);
+        secondary2 = new ColorUIResource(this.F);
+        primary1 = new ColorUIResource(this.C);
+        secondary1 = new ColorUIResource(this.C);
+        primary2 = new ColorUIResource(this.B);
+        primary3 = new ColorUIResource(this.E);
+    }
+
+    @Override
+    public void addCustomEntriesToTable(UIDefaults table) {
+        super.addCustomEntriesToTable(table);       
+    }
+
+    @Override
+    public String getName() {
+        return "Khaki";
+    }
+
+    @Override
+    public ColorUIResource getPrimary1() {
+        return primary1;
+    }
+
+    @Override
+    public ColorUIResource getPrimary2() {
+        return primary2;
+    }
+
+    @Override
+    public ColorUIResource getPrimary3() {
+        return primary3;
+    }
+
+    @Override
+    public ColorUIResource getSecondary1() {
+        return secondary1;
+    }
+
+    @Override
+    public ColorUIResource getSecondary2() {
+        return secondary2;
+    }
+
+    @Override
+    public ColorUIResource getSecondary3() {
+        return secondary3;
+    }
+
+    @Override
+    public FontUIResource getControlTextFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getSystemTextFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getUserTextFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getMenuTextFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getWindowTitleFont() {
+        return fontUIResource;
+    }
+
+    @Override
+    public FontUIResource getSubTextFont() {
+        return fontUIResource;
+    }
+
+}
