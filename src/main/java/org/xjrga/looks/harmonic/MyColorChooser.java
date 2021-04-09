0d8973044abb59efb2b5d41657ef577ef484a1bf
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2021-04-09 15:36:17 -0500

    wip

--- src/main/java/org/xjrga/looks/harmonic/MyColorChooser.java
+++ src/main/java/org/xjrga/looks/harmonic/MyColorChooser.java
@@ -51,8 +51,7 @@ public class MyColorChooser {
         panel.setPreferredSize(new Dimension(600, 200));
         panel.setLayout(layout);
         panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-        JLabel label = new JLabel();        
-        label.setOpaque(true);
+        JLabel label = new JLabel();                
         panel.add(label);
         JScrollPane jScrollPane = new JScrollPane(panel);  
         jScrollPane.setViewportView(panel);
