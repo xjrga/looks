812dda45fc02c5c7517809a9f490d88c97a9843e
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2021-04-12 14:48:51 -0500

    wip

---
+++ src/main/java/org/xjrga/looks/paletteviewer/RecentColorsQueue.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2021 Jorge R Garcia de Alba &lt;jorge.r.garciadealba@gmail.com&gt;
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.xjrga.looks.paletteviewer;
+
+import java.util.LinkedList;
+
+/**
+ *
+ *
+ * @author Jorge R Garcia de Alba &lt;jorge.r.garciadealba@gmail.com&gt;
+ * @param <Color>
+ */
+public class RecentColorsQueue<Color> {
+
+    private final LinkedList<Color> list;
+
+    public RecentColorsQueue() {
+        list = new LinkedList();
+    }
+
+    public Color getHead() {
+        return list.poll();
+    }
+
+    public void enqueue(Color color) {
+        list.add(color);
+        if (list.size() > 35) {
+            dequeue();
+        }
+    }
+
+    public Color dequeue() {
+        return list.remove(list.size() - 1);
+    }
+}
