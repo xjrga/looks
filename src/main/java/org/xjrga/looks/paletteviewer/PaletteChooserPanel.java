37aa7b46ee13d516f2a10d89016839fe6ed061b0
Jorge Garcia de Alba <jorge.r.garciadealba@gmail.com>
2021-04-13 17:28:05 -0500

    wip

--- src/main/java/org/xjrga/looks/paletteviewer/PaletteChooserPanel.java
+++ src/main/java/org/xjrga/looks/paletteviewer/PaletteChooserPanel.java
@@ -48,7 +48,7 @@ public class PaletteChooserPanel extends AbstractColorChooserPanel {
         setLayout(new BorderLayout(10,10));
         textFieldPaletteName = new JTextField();
         panelResult = new JPanel();        
-        textFieldPaletteName.setPreferredSize(new Dimension(300, 28));
+        textFieldPaletteName.setPreferredSize(new Dimension(375, 28));
         add(panelResult,BorderLayout.CENTER);
         JPanel textFieldPanel = new JPanel();        
         textFieldPanel.add(new JLabel("Palette: "));
